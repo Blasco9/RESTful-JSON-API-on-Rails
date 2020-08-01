2e263eef9557ca5d96c14632e12da922ec4fe753
Julian Blasco <blascoblasco9@gmail.com>
2020-08-01 16:14:44 -0300

    Write model tests for asociations and validations and modify those models to pass the tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Validations' do
+    it { should validate_presence_of(:name) }
+  end
+
+  context 'Asociations' do
+    it { should belong_to(:todo) }
+  end
 end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Validations' do
+    it { should validate_presence_of(:title) }
+    it { should validate_presence_of(:created_by) }
+  end
+
+  context 'Asociations' do
+    it { should have_many(:items).dependent(:destroy) }
+  end
 end
