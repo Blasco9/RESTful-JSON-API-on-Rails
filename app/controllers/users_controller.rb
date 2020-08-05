d3208d2df7085ebd4f87763dfd8144d60fa53a26
Julian Blasco <blascoblasco9@gmail.com>
2020-08-05 16:47:09 -0300

    Write UserController tests. Create UserController and signup route

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,15 @@
+class UsersController < ApplicationController
+  # return authenticated token upon signup
+  def create
+    user = User.create!(user_params)
+    auth_token = AuthenticateUser.new(user.email, user.password).call
+    response = { message: Message.account_created, auth_token: auth_token }
+    json_response(response, :created)
+  end
+
+  private
+
+    def user_params
+      params.permit(:name, :email, :password, :password_confirmation)
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
     resources :items
   end
   post 'auth/login', to: 'authentication#authenticate'
+  post '/signup', to: 'users#create'
 end
